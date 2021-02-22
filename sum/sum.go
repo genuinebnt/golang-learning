c9e7987fc8771186293bae8ce8aebab7947e058d
genuinebnt <genuine.basilnt@gmail.com>
2021-02-22 15:42:27 +0530

    refactored

--- sum/sum.go
+++ sum/sum.go
@@ -1,9 +1,27 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i:=0; i< 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sum := make([]int, lengthOfNumbers)
+	for i, numbers := range numbersToSum {
+		sum[i] = Sum(numbers)
+	}
+	return sum
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+	return sums
 }
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,19 +1,48 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-  numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-  got := Sum(numbers)
-  want := 15
+		got := Sum(numbers)
+		want := 15
 
-  if got != want {
-    t.Errorf("got %d want %d", got, want)
-  }
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("collection of any n numbers", func(t *testing.T) {
+		numbers := []int{3, 2, 5}
+
+		got := Sum(numbers)
+		want := 10
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("takes slices and return new slice", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{2, 3, 4})
+		want := []int{3, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("sum of slices of tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{2, 4, 2}, []int{1}, []int{5, 3}, []int{3})
+		want := []int{2, 6, 0, 3, 0}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
-
-
-
-
-
